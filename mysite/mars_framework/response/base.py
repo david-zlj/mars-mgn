026a33aeaec56533c60fe2ef2c616bb987ed605c
xwz <xwz2048@qq.com>
2025-07-02 08:53:12 +0800

    refactor(framework): 调整业务状态码范围并优化响应类注释
    - 将业务状态码范围从"111000 - 999999"调整为"100000 - 999999"
    - 更新了业务状态码分配规则和具体分配说明
    - 优化了 CommonResponse 类中 success 和 error 方法的注释，使其更加清晰

--- mysite/mars_framework/response/base.py
+++ mysite/mars_framework/response/base.py
@@ -3,30 +3,23 @@ from rest_framework import status
 
 
 class CommonResponse:
-    """
-    通用响应工具类
-    """
+    """通用响应工具类"""
 
     @staticmethod
     def success(code=0, data=True, msg="", status_code=status.HTTP_200_OK):
-        """
-        成功响应
-        """
+        """业务成功响应"""
         return Response(
             {
-                "code": code,  # 0 表示成功
+                "code": code,  # 业务状态码为 0 表示成功
                 "data": data,
                 "msg": msg,
             },
-            status=status_code,
+            status=status_code,  # HTTP 状态码默认为200
         )
 
     @staticmethod
     def error(code=500, data=None, msg="Error", status_code=status.HTTP_200_OK):
-        """
-        错误响应，用于发生业务错误
-        业务错误码参考 mars_framework.exceptions.enums.py
-        """
+        """业务错误响应，配合业务状态码使用"""
         return Response(
             {
                 "code": code,
